ac4aad36825282d2883afb6c1d8179c6c1ea040c
Jippylong12 <markysalinas12@gmail.com>
2016-01-22 22:47:06 -0600

    tried to set up a parser class and main

--- DB Management Systems/Project1/main.cpp
+++ DB Management Systems/Project1/main.cpp
@@ -5,4 +5,37 @@ Paul Park
 Clifton Sims */
 
 
+#include <iostream>
+#include <string>
+#include <sstream>
 
+using namespace std;
+
+class Parser
+{
+	stringstream input;
+public:
+	Parser(stringstream);
+
+};
+
+Parser::Parser(stringstream in)
+{
+	input = in;
+}
+
+
+
+int main()
+{
+	cout << "Staring \n" << endl;
+	cout << "Enter command: \n" << endl;
+	string mainInput;
+	stringstream command;
+	while (true)
+	{
+		cin >> mainInput;
+		command << mainInput;
+		Parser mainParser(command)
+	}
+}
