e349278c4dc6ae43dcbb3b41fa7c7158d4f5df74
Brian Ta <froob@tamu.edu>
2016-01-24 11:13:23 -0600

    Update main.cpp
    Forward declaration of DB queries and commands (not necessarily void without arguments)

--- DB Management Systems/Project1/main.cpp
+++ DB Management Systems/Project1/main.cpp
@@ -4,13 +4,35 @@ Brian Ta
 Deokman Park
 Clifton Sims */
 
-
 #include <iostream>
 #include <string>
 #include <cstring>
 #include <sstream>
 
 using namespace std;
+
+
+//-------Database queries--------//
+	void SELECT();
+	void PROJECT();
+	void RENAME();
+	void SET_UNION();
+	void SET_DIFFERENCE();
+	void CROSS_PRODUCT();
+//-------Database command functions-------//
+
+/*command ::= ( open-cmd | close-cmd | save-cmd | exit-cmd | show-cmd | create-cmd | update-cmd | insert-cmd | delete-cmd );*/
+	void open_cmd();  	//::== OPEN relation-name 
+	void close_cmd(); 	//::== CLOSE relation-name 
+	void save_cmd();  	//::== SAVE relation-name 
+	void exit_cmd();	//::== EXIT 
+	void show_cmd();    	//::== SHOW atomic-expr 
+	void create_cmd();	//::= CREATE TABLE relation-name ( typed-attribute-list ) PRIMARY KEY ( attribute-list )
+	void update_cmd();	//::= UPDATE relation-name SET attribute-name = literal { , attribute-name = literal } WHERE condition 
+	void insert_cmd();	//::= INSERT INTO relation-name VALUES FROM ( literal { , literal } ) | INSERT INTO relation-name VALUES FROM RELATION expr
+	void delete_cmd();	//::= DELETE FROM relation-name WHERE condition
+
+//-------Parser
 
 class Parser
 {
