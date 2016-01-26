f38a1dfe11790ae6a2fc5085139b9573cc94fd22
Marcus Salinas <crazymonkeyshicken@gmail.com>
2016-01-26 17:13:06 +0000

    minor chnages

---
+++ DB Management Systems/Project1/DBsytem.cpp
@@ -0,0 +1,52 @@
+/* _authors_
+Marcus Salinas
+Brian Ta
+Deokman Park
+Clifton Sims
+Ivan Rupert */
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include "Parser.h"
+
+using namespace std;
+
+
+//-------Database queries--------//
+	void SELECT();
+	void PROJECT();
+	void RENAME();
+	void SET_UNION();
+	void SET_DIFFERENCE();
+	void CROSS_PRODUCT();
+//-------Database command functions-------//
+
+/*command ::= ( open-cmd | close-cmd | save-cmd | exit-cmd | show-cmd | create-cmd | update-cmd | insert-cmd | delete-cmd );*/
+	void open_cmd();  	//::== OPEN relation-name 
+	void close_cmd(); 	//::== CLOSE relation-name 
+	void save_cmd();  	//::== SAVE relation-name 
+	void exit_cmd();	//::== EXIT 
+	void show_cmd();    	//::== SHOW atomic-expr 
+	void create_cmd();	//::= CREATE TABLE relation-name ( typed-attribute-list ) PRIMARY KEY ( attribute-list )
+	void update_cmd();	//::= UPDATE relation-name SET attribute-name = literal { , attribute-name = literal } WHERE condition 
+	void insert_cmd();	//::= INSERT INTO relation-name VALUES FROM ( literal { , literal } ) | INSERT INTO relation-name VALUES FROM RELATION expr
+	void delete_cmd();	//::= DELETE FROM relation-name WHERE condition
+
+
+
+
+
+int main()
+{
+	cout << "Starting..." << endl;
+	cout << "Enter command: " << endl;
+	string mainInput;
+	while (true)
+	{
+		getline(cin, mainInput);
+		Parser mainParser(mainInput);
+		mainParser.parse();
+	}
+}
