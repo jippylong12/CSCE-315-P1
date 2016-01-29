207da88d360acf534d4f829f7e857f9c16eed4c9
Marcus Salinas <crazymonkeyshicken@gmail.com>
2016-01-29 18:26:34 +0000

    The changes I can remember is making this compilable agian. We did work together in lab on the 27th for the functions.

--- DB Management Systems/Project1/Table.cpp
+++ DB Management Systems/Project1/Table.cpp
@@ -5,6 +5,25 @@
 using namespace std;
 
 
+Table::Table(const Table &Source)
+{
+	table = Source.table;
+	tableName = Source.tableName;
+	rowLength = Source.rowLength;
+	columnLength = Source.columnLength;
+	primaryKeys = Source.primaryKeys;
+}
+
+Table& Table::operator= (const Table &Source)
+{
+	table = Source.table;
+	tableName = Source.tableName;
+	rowLength = Source.rowLength;
+	columnLength = Source.columnLength;
+	primaryKeys = Source.primaryKeys;
+	return *this;
+}
+
 Table::Table(int rows, int columns,string name, vector<string> headers, vector<string> keys) //constructor
 {
 	rowLength = rows; //set the number of rows
@@ -28,107 +47,4 @@ Table::Table() //defualt constructor
 	primaryKeys.resize(1);//resize to one
 	primaryKeys[0] = "default"; //set default primary keys
 }
-
-Table* Table::open_cmd(string nameOpen) //bring a table into memory from file 
-{
-	//read in from the comma seperated text file
-	//the numbers of rows and columns should be the first two lines of every file
-	//we create a table with those dimensions
-	//change the values of the newly created table in memory by reading from file.
-	//return 0;  //I just think it's better to return 0 on success rather than 1.
-}
-
-int Table::close_cmd(string nameClose) //saves and removes table instance from memory
-{
-	//create a new txt file with the name of the file
-	//read in the rowLength and columnLenght of table first
-	//read in row by row into file
-	//remove from memory
-	//return 0;
-}
-
-int Table::save_cmd(string nameSave) //save the table to file keep in memory
-{
-	//create a new txt file with the name of the file
-	//read in the rowLength and columnLenght of table first
-	//read in row by row into file
-	//close file
-	//return 0;
-}
-	
-	
-//Don't think that this needs the input of string either.  If it's accessing the current table in memory, I don' think it needs a string argument.
-int Table::show_cmd(string nameShow) //print out the table currently in memory
-{
-	//nested for loop
-	//print each cell in the table
-	//return 0;
-	
-	//First, print the name of the table
-	cout<<nameShow<<endl<<endl;
-	//iterate through the table and print.
-	for (int i = 0; i<rowLength; ++i)
-	{
-		for (int j = 0; j<columnLength; ++j)
-		{
-			cout<<table[i][j]<<"  ";
-		}
-		cout<<endl;
-	}
-	return 0;
-	
-	
-}
-
-Table* Table::create_cmd(int rowCreate, int columnCreate, string nameCreate,vector<string> createHeaders, vector<string> createKeys) //create a new table in memory
-{
-	//intiliaze new Table
-	Table* newTable = new Table(rowCreate, columnCreate,nameCreate,createHeaders, createKeys)
-	//return new Table
-	return newTable;
-	
-}
-
-//updates a record in the database given certain criteria
-int Table::update_cmd(string nameUpdate, string headerName, string criteria, string replace)
-{
-	//go to table nameUpdate
-	//search first row for headerName to find column
-	//search that column for the criteria
-	//replace the criteria with replace
-	//return 0;
-}
-
-//Takes in the input of nameInsert and returns an int for error checking.
-int Table::insert_cmd(string nameInsert, int rowInsert, int colInsert)
-{
-	//Check if the rowInsert and colInsert are within the bounds of the table.
-    if (rowInsert < rowLength && colInsert < columnLength)
-    {
-        table[rowInsert][colInsert] = nameInsert;
-        return 0;
-    }
-    //Else returns 1 for failure.
-    else
-    {
-    
-        return 1;
-    }
-}
-
-//nameDelete may not be neede depending on the implementation of the Parser.
-//Parser may end up finding the row and may only need one input here.
-int Table::delete_cmd(string nameDelete, int rowDelete)
-{
-	//Check for out of bounds error
-    if (rowDelete < rowLength)
-    {
-        table.erase(table.begin() + rowDelete);
-        return 0;
-    }
-    //If out of bounds, return 1 for failure.
-    else
-    {
-        return 1;
-    }
-}
+//Table::~Table(){}
--- DB Management Systems/Project1/Table.h
+++ DB Management Systems/Project1/Table.h
@@ -17,22 +17,16 @@ public:
 
 	/*command ::= ( open-cmd | close-cmd | save-cmd | exit-cmd | show-cmd | create-cmd | update-cmd | insert-cmd | delete-cmd );*/
 	//Table Constructor
-
 	Table(int,int,string, vector<string>, vector<string>); //constructor	
 	Table(); 			//default
-	Table* open_cmd(string);  	//::== OPEN relation-name 
-	int close_cmd(string); 	//::== CLOSE relation-name 
-	int save_cmd(string);  	//::== SAVE relation-name 
-	int show_cmd(string);    	//::== SHOW atomic-expr 
-	Table* create_cmd(string, vector<string>);	//::= CREATE TABLE relation-name ( typed-attribute-list ) PRIMARY KEY ( attribute-list )
-	int update_cmd(string, string, string, string);	//::= UPDATE relation-name SET attribute-name = literal { , attribute-name = literal } WHERE condition 
-	int insert_cmd(string, int , int );	//::= INSERT INTO relation-name VALUES FROM ( literal { , literal } ) | INSERT INTO relation-name VALUES FROM RELATION expr
-	int delete_cmd(string , int );	//::= DELETE FROM relation-name WHERE condition
+	Table(const Table &Source); //copy constructor
+	Table& operator= (const Table &Source); //overloading
 	int getRowLength() { return rowLength;} 
 	int getColumnLength() { return columnLength;}
 	vector<string> getPrimnaryKey() { return primaryKeys;} //return key vector
 	void setPrimaryKey(vector<string> setKeys) { primaryKeys = setKeys;} //set any of the values of the primary key
 	string getTableName() { return tableName;}
+	vector< vector<string> > getTable() {return table;}
 	
 };
 
